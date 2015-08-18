406c78514106505a2593a169798158ccc43db46a
Martyn Bissett <martynbissett@yahoo.co.uk>
2015-08-18 22:20:41 +0900

    Add adapter abstract and interface

--- files/templates/scan.php
+++ files/templates/scan.php
@@ -1,11 +1,4 @@
-<?php
-
-use Cattlog\Output;
-
-// don't filter files that don't exist, we want to show those too
-$destFiles = $fileSystem->getDestFiles($lang);
-
-?>Checking...
+Checking...
 <?php foreach ($destFiles as $file): ?>
 <?php if ($fileSystem->fileExists($file)): ?>
     <?php echo $file . PHP_EOL; ?>
@@ -14,15 +7,7 @@ $destFiles = $fileSystem->getDestFiles($lang);
 <?php endif; ?>
 <?php endforeach; ?>
 
-<?php
-
-$keysFromDest = $cattlog->getKeysFromDestFiles($lang);
-$keysFromSrc = $cattlog->getKeysFromSrcFiles();
-
-$keysToAdd = $cattlog->getAddedKeys($keysFromDest, $keysFromSrc);
-$keysToRemove = $cattlog->getRemovedKeys($keysFromDest, $keysFromSrc);
-
-if (count($keysToAdd)): ?>
+<?php if (count($keysToAdd)): ?>
 The following new keys were found:
 <?php foreach ($keysToAdd as $value): ?>
     <?php echo Output::_($value, Output::BG_GREEN) . PHP_EOL; ?>
